839913c4cd3f4dd84a728944ce5ab5202ad73a4f
hannaskliarova <anyasklyarova4898@gmail.com>
2023-03-02 19:51:04 +0200

    Homework 2 Part 1

--- Homework2(Lesson3)/Program.cs
+++ Homework2(Lesson3)/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //P.1
+            /*double x, result1, result2, result3, result4, result; 
+            Console.Write("Enter the x parameter: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            result1 = Math.Pow(x, (double) 3 / 1); 
+            result2 = result1 * -6;
+            result3 = Math.Pow(x, (double) 2 / 1) * 5;
+            result4 = x * 10;
+            result = result2 + result3 - result4 + 15;
+            Console.WriteLine(result);*/
+
+
+            //P.2
+            /*double x, y;
+            x = 5;
+            y = Math.Abs(x) * Math.Sin(x);
+            Console.WriteLine(y);*/
+
+
+            //P.3
+            /*double pi = (double)System.Math.PI;
+            int x;
+            Console.Write("Enter the x parameter: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            double result = 2 * pi * x;
+            Console.WriteLine(result);*/
+
+
+            //P.4
+            /*int x, y, result;
+            x = 3; y = 5;
+            result = Math.Max(x, y);
+            Console.WriteLine(result);*/
+
+           
+
+
+        
+
+
+
+
+
+
+        }
+    }
+}
