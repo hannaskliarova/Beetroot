0c28d9ddba9e176f28b562d3d99ee5d545dced1f
hannaskliarova <anyasklyarova4898@gmail.com>
2023-03-05 21:25:19 +0200

    Homework3

---
+++ Homework3(Lesson4)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Enter x parameter: ");
+        int parameterX = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(parameterX);
+
+        Console.Write("Enter y parameter: ");
+        int parameterY = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(parameterY);
+        
+        int totalSumFormula = (parameterY* (parameterY + 1)) / 2 - ((parameterX - 1) * parameterX) / 2;
+
+            if (parameterX != parameterY)
+
+            {
+                Console.WriteLine(totalSumFormula);
+            }
+            
+            else if (parameterY == parameterX)
+            // но я не вижу смысла прописывать это условие, потому что при x == y, sum всегда будет x&&y?
+            {
+                Console.WriteLine("The parameters are equal");
+            }
+
+            
+        }
+    }
+}
