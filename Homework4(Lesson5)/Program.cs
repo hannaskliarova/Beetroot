700104b0de62ef00c58575d4787d006f2acb8ff6
hannaskliarova <anyasklyarova4898@gmail.com>
2023-03-07 21:17:17 +0200

    Commit

---
+++ Homework4(Lesson5)/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework4_Lesson5_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+
+            Console.WriteLine(int);
+        }
+    }
+}
