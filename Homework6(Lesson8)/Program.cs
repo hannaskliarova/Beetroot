15a7b62b8a086e19eb727178318d5d601f211ffc
hannaskliarova <anyasklyarova4898@gmail.com>
2023-03-14 21:57:46 +0200

    Pt.1

--- Homework6(Lesson8)/Program.cs
+++ Homework6(Lesson8)/Program.cs
@@ -2,9 +2,50 @@
 {
     internal class Program
     {
+        //P.1
+        static bool CompareTwoArrays(char[] a, char[] b)
+        {
+            if (a.Length != b.Length) 
+                { 
+                    return false; 
+                }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i]) 
+                { 
+                    return false; 
+                }
+            }
+            return true;
+
+            Console.WriteLine(CompareTwoArrays);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //P.1
+            string FirstString, SecondString;
+            string FirstStringToLower, SecondStringToLower;
+            bool result;
+            
+            Console.Write("Enter the First string: ");
+            FirstString= Console.ReadLine();
+
+            Console.Write("Enter the Second string: ");
+            SecondString= Console.ReadLine();
+
+            FirstStringToLower = FirstString.ToLower ();
+            SecondStringToLower = SecondString.ToLower();
+
+            char[] FirstStringArray = FirstStringToLower.ToCharArray();
+            char[] SecondStringArray = SecondStringToLower.ToCharArray();
+
+            result = CompareTwoArrays(FirstStringArray, SecondStringArray);
+
+            Console.WriteLine(result);
+            
+
+
         }
     }
 }
