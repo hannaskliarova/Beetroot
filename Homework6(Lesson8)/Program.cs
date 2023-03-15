c1ce22f50339a94ac8919204daf48fbe6c75d258
hannaskliarova <anyasklyarova4898@gmail.com>
2023-03-15 17:26:12 +0200

    Homework without P.4

--- Homework6(Lesson8)/Program.cs
+++ Homework6(Lesson8)/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Homework6_Lesson8_
+﻿using System;
+namespace Homework6_Lesson8_
 {
     internal class Program
     {
         //P.1
-        static bool CompareTwoArrays(char[] a, char[] b)
+
+
+        /*static bool CompareTwoArrays(char[] a, char[] b)
         {
             if (a.Length != b.Length) 
                 { 
@@ -19,33 +22,154 @@
             return true;
 
             Console.WriteLine(CompareTwoArrays);
-        }
-        static void Main(string[] args)
+        }*/
+
+        
+        //P.2 (attempt 1)
+
+
+        /*static int Analyze (string stringAnalysis)
         {
             
-            //P.1
-            string FirstString, SecondString;
-            string FirstStringToLower, SecondStringToLower;
-            bool result;
+            int alphabet, digit, specialchar, i, l;
+            alphabet = digit = specialchar = i = 0;
+            l = stringAnalysis.Length;
             
-            Console.Write("Enter the First string: ");
-            FirstString= Console.ReadLine();
 
-            Console.Write("Enter the Second string: ");
-            SecondString= Console.ReadLine();
+            while (i < l)
+            {
+                if ((stringAnalysis[i] >= 'a' && stringAnalysis[i] <= 'z') || (stringAnalysis[i] >= 'A' && stringAnalysis[i] <= 'Z'))
+                {
+                    alphabet++;
+                }
+                else if (stringAnalysis[i] >= '0' && stringAnalysis[i] <= '9')
+                {
+                    digit++;
+                }
+                else
+                {
+                    specialchar++;
+                }
 
-            FirstStringToLower = FirstString.ToLower ();
-            SecondStringToLower = SecondString.ToLower();
-
-            char[] FirstStringArray = FirstStringToLower.ToCharArray();
-            char[] SecondStringArray = SecondStringToLower.ToCharArray();
-
-            result = CompareTwoArrays(FirstStringArray, SecondStringArray);
-
-            Console.WriteLine(result);
+                i++;
+            }
+            return (alphabet, digit, specialchar).digit ;
             
+        }*/
+
+        //P.2 (Attempt 2)
+
+
+        /*static string SortedString (string EnteredString)
+         {
+             string temp;
+             char[] charString = EnteredString.ToCharArray();
+             for (int i = 1; i < charString.Length; i++)
+             {
+                 for (int j = 0; j < charString.Length - 1; j++)
+                 {
+                     if (charString[j] > charString[j + 1])
+                     {
+                         temp = charString[j];
+                         charString[j] = charString[j + 1];
+                         charString[j + 1] = temp;
+                     }
+                 }
+             }
+             return charString;  
+         }*/
+
+        
+        //P.3 Sort that will return string that contains all characters from input string sorted in alphabetical order (e.g. 'Hello' -> 'ehllo')
+        
+        
+        /*static string SortedString (string enteredString)
+        {
+            
+            char[] characters = enteredString.ToArray();
+            Array.Sort(characters);
+            return new string(characters);
+            
+        }*/
+
+        //P.4
+
+
+        
+            static void Main(string[] args)
+        {
+
+            //P.1 Compare that will return true if 2 strings are equal, otherwise false, but do not use build-in method
+
+            /*string FirstString, SecondString;
+             string FirstStringToLower, SecondStringToLower;
+             bool result;
+
+             Console.Write("Enter the First string: ");
+             FirstString= Console.ReadLine();
+
+             Console.Write("Enter the Second string: ");
+             SecondString= Console.ReadLine();
+
+             FirstStringToLower = FirstString.ToLower ();
+             SecondStringToLower = SecondString.ToLower();
+
+             char[] FirstStringArray = FirstStringToLower.ToCharArray();
+             char[] SecondStringArray = SecondStringToLower.ToCharArray();
+
+             result = CompareTwoArrays(FirstStringArray, SecondStringArray);
+
+             Console.WriteLine(result);*/
+
+            //P.2
+
+            /*string str;
+            int alphabet, digit, specialchar, i, l;
+            alphabet = digit = specialchar = i = 0;
+
+            Console.Write("Enter the string : ");
+            str = Console.ReadLine();
+            l = str.Length;
+
+            while (i < l)
+            {
+                if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                    alphabet++;
+                }
+                else if (str[i] >= '0' && str[i] <= '9')
+                {
+                    digit++;
+                }
+                else
+                {
+                    specialchar++;
+                }
+
+                i++;
+            }
+
+
+            Console.Write("Number of Alphabets in the string is : {0}\n", alphabet); 
+            Console.Write("Number of Digits in the string is : {0}\n", digit);
+            Console.Write("Number of Special characters in the string is : {0}\n\n", specialchar);
+
+            Console.ReadLine();*/
+
+            //P.3
+
+            /*string enteredString;
+
+            Console.Write("Enter the string : ");
+            enteredString = Console.ReadLine();
+
+            string sortedNew = SortedString(enteredString);
+            Console.WriteLine(sortedNew);*/
+
+            //P.4
 
 
         }
+
     }
 }
