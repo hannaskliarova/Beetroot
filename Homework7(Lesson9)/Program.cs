a61e4e4291c60dfb3766b2b6e6ade10f809e6b62
hannaskliarova <anyasklyarova4898@gmail.com>
2023-03-16 17:18:03 +0200

    Project created

---
+++ Homework7(Lesson9)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework7_Lesson9_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
